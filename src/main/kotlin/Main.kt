e0535c6044dd0df851c2273c28cd2f6447f1b7c6
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-10-12 14:48:40 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+package my.demo
+
+import kotlin.text.*
+/* //dozen toutorial in 9 line
+const val apples =50
+const val dozens =12 */
+
+fun main(args: Array<String>) {
+/* //dozen toutorial in 4 line
+    var no0fdozens = apples / dozens
+    var remainapples = apples  % dozens // can i swich var to val no problem
+
+
+    println(no0fdozens)
+    println(remainapples)
+*/
+
+
+
+   /* //10/10/2021
+   println("Hello Haya")
+    // Try adding program arguments at Run/Debug configuration
+   println("Program arguments: ${args.joinToString()}")*/
+
+/* //12/10/2021
+    var name ="haya"
+    var score=5
+    score=score+3 //same score+=3 and all operator have same way operator mean (* ,- ,+, %, )
+
+    //when i add this " score = score +3 will print 11 number
+    println(score)
+   */
+
+   /*
+    var phone=111
+    phone= 9999
+
+    val xphone=3333 //here i cant do like var i can't updat the value in code like what i do in 21 line 
+
+    */
+
+
+
+}
